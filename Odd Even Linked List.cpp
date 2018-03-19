0838f36929ec4eeaa889fcb098986f631647e798
xxxx2013 <472224334@qq.com>
2018-03-19 09:39:28 +0800

    xuxin

--- Odd Even Linked List.cpp
+++ Odd Even Linked List.cpp
@@ -6,7 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-//分成两个链表，然后分别把位置为奇数的归一类，把位置为偶数的归为一类，最后把位置为偶数的链表接在位置为奇数链表的后面。
+//创建两个链表，然后分别把位置为奇数的归为一个链表，把位置为偶数的归为一个链表，最后把位置为偶数的链表接在位置为奇数链表的后面。
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {

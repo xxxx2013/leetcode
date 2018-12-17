84b033d09a00323dcdcf0406ca19326a878e4322
xuxin <xuxin@example.com>
2018-12-17 14:07:08 +0800

    xuxin

--- Linked List Cycle.cpp
+++ Linked List Cycle.cpp
@@ -10,19 +10,17 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         
-        ListNode *tail = head;
-        ListNode *p = head;
-        if(head == NULL)
-            return false;
-        if(head->next == NULL)
-            return false;
-        while(p != NULL && p->next != NULL)
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast!=NULL && fast->next!=NULL)
         {
-            p = p->next->next;
-            tail = tail->next;
-            if(p == tail)
+            slow = slow->next;
+            fast = fast->next;
+            fast = fast->next;
+            if(slow==fast)
                 return true;
         }
+        
         return false;
     }
 };

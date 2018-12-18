92687f39fdbbc9def250eada5239dfb79ad8b2ae
xuxin <xuxin@example.com>
2018-12-18 11:12:56 +0800

    xuxin

--- Sort List.cpp
+++ Sort List.cpp
@@ -6,6 +6,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ 
+//1.vector数组法
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
@@ -30,4 +32,52 @@ public:
         }
         return res->next;
     }
+};
+
+//2.链表归并排序法
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+        if(head==NULL)
+            return head;
+        if(head->next==NULL)
+            return head;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* pre = head;
+        while(fast!=NULL&&fast->next!=NULL)
+        {
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next;
+            fast = fast->next;
+        }
+        pre->next = NULL;
+        return merge(sortList(head),sortList(slow));
+    }
+    ListNode* merge(ListNode* l1,ListNode* l2)
+    {
+        ListNode* dummy = new ListNode(NULL);
+        ListNode* cur = dummy;
+        while(l1!=NULL && l2!=NULL)
+        {
+            if(l1->val<l2->val)
+            {
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        if(l1!=NULL) cur->next = l1;
+        if(l2!=NULL) cur->next = l2;
+        return dummy->next;
+    }
+    
 };

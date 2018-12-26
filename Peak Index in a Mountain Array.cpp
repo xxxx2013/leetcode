74509bd42c696087a72f56443fe7bef78987f65d
xuxin <xuxin@example.com>
2018-12-26 11:44:22 +0800

    xuxin

---
+++ Peak Index in a Mountain Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        
+        int max = A[0];
+        int index = 0;
+        for(int i=0; i<A.size(); i++)
+        {
+            if(A[i]>max)
+            {
+                max = A[i];
+                index = i;
+            }
+        }
+        return index;
+        
+    }
+};

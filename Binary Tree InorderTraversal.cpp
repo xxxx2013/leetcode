f451bccf92a91e2a5f85d8453fe03039ecee6cf0
xxxx2013 <472224334@qq.com>
2018-12-29 23:34:48 +0800

    xuxin

--- Binary Tree InorderTraversal.cpp
+++ Binary Tree InorderTraversal.cpp
@@ -1,30 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
+    
+    struct Command
+    {
+        TreeNode *node;
+        string s;
+        Command(string s,TreeNode* node):s(s),node(node){}
+    };
+    
     vector<int> inorderTraversal(TreeNode* root) {
         
-        vector <int> res;
-        stack <TreeNode *> s;
-        TreeNode *p = root;
+        vector<int> res;
+        if(!root)
+            return res;
         
-    while(!s.empty() || p != nullptr)
-
-    {        
-        while(p != nullptr)
+        stack <Command> stc;
+        stc.push(Command("go",root));
+        
+        while(stc.empty()==false)
         {
-            s.push(p);
-            p = p -> left;
+            Command command = stc.top();
+            stc.pop();
+            if(command.s=="print")
+            {
+                res.push_back(command.node->val);
+            }
+            else
+            {
+                if(command.node->right)
+                {
+                    stc.push(Command("go",command.node->right));
+                }
+                stc.push(Command("print",command.node));
+                if(command.node->left)
+                {
+                    stc.push(Command("go",command.node->left));
+                }
+                
+            }
         }
         
-        if(!s.empty())
-        {
-            p = s.top();
-            s.pop();
-            
-            res.push_back(p -> val);
-            p = p -> right;
-        }
-        
-    }        
         return res;
         
     }

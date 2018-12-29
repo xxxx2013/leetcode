a2bd1511a889ca69449468acaddd8cfb782c4f5f
xxxx2013 <472224334@qq.com>
2018-12-29 19:10:06 +0800

    xuxin

--- Valid Palindrome.cpp
+++ Valid Palindrome.cpp
@@ -58,3 +58,44 @@ public:
         return true;
     }
 };
+
+class Solution {
+    //采用Stack方法
+public:
+    bool isValid(string s) {
+        
+        if(s.size()==0)
+            return true;
+        stack<int> stack;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+                stack.push(s[i]);
+            else
+            {
+                if(stack.size()==0)
+                    return false;
+                char c = stack.top();
+                stack.pop();
+                char match;
+                switch(s[i])
+                {
+                    case ')':
+                        match = '(';
+                        break;
+                    case ']':
+                        match = '[';
+                        break;
+                    case '}':
+                        match = '{';
+                        break;
+                }
+                if(c!=match)
+                    return false;
+            }
+        }
+        if(stack.size()!=0)
+            return false;
+        return true;
+    }
+};

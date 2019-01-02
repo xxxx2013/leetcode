f8a44f9a642cdaa01f1f283689b1d115420f7800
xuxin <xuxin@example.com>
2019-01-02 08:54:24 +0800

    xuxin

---
+++ Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    struct Command
+    {
+        string s;
+        TreeNode* node;
+        Command(string s, TreeNode* node):s(s),node(node){}
+    };
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+        vector<int> res;
+        if(!root)
+            return res;
+        
+        stack <Command> stc;
+        stc.push(Command("go",root));
+        while(stc.empty()==false)
+        {
+            Command command = stc.top();
+            stc.pop();
+            if(command.s=="print")
+            {
+                res.push_back(command.node->val);
+            }
+            else
+            {
+                stc.push(Command("print",command.node));
+                if(command.node->right)
+                {
+                    stc.push(Command("go",command.node->right));
+                }
+                
+                if(command.node->left)
+                {
+                    stc.push(Command("go",command.node->left));
+                }
+                
+                
+            }
+            
+            
+        }
+        return res;
+    }
+};

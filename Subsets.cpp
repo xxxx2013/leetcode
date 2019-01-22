5c5f5155f37d50ac7a94be5e8c13a8d5d09bfae7
xuxin <xuxin@example.com>
2019-01-22 09:33:23 +0800

    xuxin

--- Subsets.cpp
+++ Subsets.cpp
@@ -30,3 +30,30 @@ public:
         
     }
 };
+
+
+class Solution {
+    //非递归算法求解
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> res(1);
+        if(nums.size()==0)
+            return res;
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            //res.push_back(nums[i]);
+            int size = res.size();
+            for(int j=0; j<size; j++)
+            {
+                res.push_back(res[j]);
+                res.back().push_back(nums[i]);
+            }
+        }
+        return res;
+    }
+    
+};

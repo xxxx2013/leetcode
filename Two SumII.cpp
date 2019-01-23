7e398c55854afb2dbb7a0a923290765587b220a8
xuxin <xuxin@example.com>
2019-01-23 14:56:47 +0800

    xuxin

--- Two SumII.cpp
+++ Two SumII.cpp
@@ -1,4 +1,5 @@
 class Solution {
+	//双指针法
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         
@@ -23,6 +24,34 @@ public:
                 j--;
             }
         }
+        return res;
+    }
+};
+
+class Solution {
+    //map<int,int>查找表法
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        vector<int> res;
+        map<int,int> rec;
+        
+        for(int i=0; i<numbers.size(); i++)
+        {
+            rec[numbers[i]] = i;
+        }
+        
+        for(int i=0; i<numbers.size(); i++)
+        {
+            map<int,int>::iterator it = rec.find(target-numbers[i]);
+            if(it!=rec.end() && it->second>i)
+            {
+                res.push_back(i+1);
+                res.push_back(it->second+1);
+                break;
+            }
+        }
+        
         return res;
     }
 };

2fef7a1d60faebab2e55a42bb7592c74917062ec
xuxin <xuxin@example.com>
2019-01-23 14:46:30 +0800

    xuxin

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -20,3 +20,31 @@ public:
        return res;
     }
 };
+
+class Solution {
+    //map查找表方法
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> res;
+        
+        map<int,int> rec;
+        for(int i=0; i<nums.size(); i++)
+        {
+            rec[nums[i]] = i;
+        }
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            map<int,int>::iterator it = rec.find(target-nums[i]);
+            if(it!=rec.end() && it->second!=i)
+            {
+                res.push_back(i);
+                res.push_back(it->second);
+                break;
+            }
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};

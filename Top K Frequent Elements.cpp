35f1ad7ef5c4f8ad8584f9e5bf24a29a2ed4af77
xuxin <xuxin@example.com>
2019-01-24 11:44:19 +0800

    xuxin

--- Top K Frequent Elements.cpp
+++ Top K Frequent Elements.cpp
@@ -1,39 +1,43 @@
 class Solution {
-    //K个元素的最小堆优先队列
+    //使用基于最小堆的priority_queue最小有限队列求解
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         map<int,int> rec;
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> p_queue;
+        
         for(int i=0; i<nums.size(); i++)
         {
             rec[nums[i]]++;
         }
         
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        
         for(map<int,int>::iterator it=rec.begin(); it!=rec.end(); it++)
         {
-            if(pq.size()==k)
+            if(p_queue.size()==k)
             {
-                if(it->second>pq.top().first)
+                if(it->second>p_queue.top().first)
                 {
-                    pq.pop();
-                    pq.push(make_pair(it->second,it->first));
+                    p_queue.pop();
+                    p_queue.push(make_pair(it->second,it->first));
                 }
             }
             else
             {
-                pq.push(make_pair(it->second,it->first));
+                p_queue.push(make_pair(it->second,it->first));
             }
-    }
-        
-        vector<int> res;
-        while(pq.empty()==false)
-        {
-            res.push_back(pq.top().second);
-            pq.pop();
+            
+            
         }
         
+        vector<int> res;
+        
+        while(p_queue.empty()==false)
+        {
+            res.push_back(p_queue.top().second);
+            p_queue.pop();
+        }
         return res;
+        
     }
 };

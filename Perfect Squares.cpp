1b0753dccd477df0cd7b40b53847f777bd1cdd93
xuxin <xuxin@example.com>
2019-02-12 09:28:07 +0800

    xuxin

--- Perfect Squares.cpp
+++ Perfect Squares.cpp
@@ -26,40 +26,6 @@ public:
 };
 
 class Solution {
-    //广度优先搜索(BFS)和图的最短路径方法
-public:
-    int numSquares(int n) {
-        
-        queue <pair<int,int>> q;
-        q.push(make_pair(n,0));
-        vector <bool> visited(n+1,false);
-        visited[n] = true;
-        
-        while(q.empty()==false)
-        {
-            int num = q.front().first;
-            int step = q.front().second;
-            q.pop();
-            
-            for(int i=0; num-i*i>=0; i++)
-            {
-                if(num-i*i==0)
-                    return step+1;
-                
-                if(visited[num-i*i] == false)
-                {
-                    q.push(make_pair(num-i*i,step+1));
-                    visited[num-i*i] = true;
-                }
-                
-            }
-        }
-     
-        return 1;
-    }
-};
-
-class Solution {
     //队列与图方法求解
 public:
     int numSquares(int n) {
